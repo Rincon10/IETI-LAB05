2c74b3d63985fdf14e782ed7f6449e6c8208666a
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-03-01 21:47:22 -0500

    Base Code Front-end

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import SignIN from 'components/singin/SignIN';
+import React from 'react';
+
+const App = () => {
+    return (
+        <main id="main-container">
+            <SignIN />
+        </main>
+    );
+};
+
+export default App;
