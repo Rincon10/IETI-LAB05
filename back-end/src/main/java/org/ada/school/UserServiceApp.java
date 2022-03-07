faca814cd59b22b922c81b38a580fa08185b3b26
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-03-06 21:20:32 -0500

    Heroku

--- back-end/src/main/java/org/ada/school/UserServiceApp.java
+++ back-end/src/main/java/org/ada/school/UserServiceApp.java
@@ -4,12 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import java.util.Collections;
+
 @SpringBootApplication
 @EnableMongoRepositories("org.ada.school.repository")
 public class UserServiceApp {
+    public static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080;
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApp.class, args);
+        SpringApplication app = new SpringApplication(UserServiceApp.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+        app.run(args);
     }
 
 }
