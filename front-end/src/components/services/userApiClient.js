d51045d9adc2a962b6405971ab2fee8b4169c85e
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-03-07 09:51:39 -0500

    userApiClient Working

--- front-end/src/components/services/userApiClient.js
+++ front-end/src/components/services/userApiClient.js
@@ -1,35 +1,19 @@
-import axios from 'axios';
-
 export const userApiclient = (() => {
-    const URL = 'https://planner-back-end.herokuapp.com';
-    /* const URL = 'http://localhost:8080'; */
+    /* const URL = 'https://planner-back-end.herokuapp.com'; */
+    const URL = 'http://localhost:8080';
+    const myHeader = new Headers();
+    myHeader.set('Content-Type', 'application/json');
+    myHeader.set('Access-Control-Allow-Origin', '*');
+
     return {
         getToken: async loginDTO => {
-            /* const response = await fetch(`${URL}/api/v1/auth`, {
-                mode: 'cors',
+            const response = await fetch(`${URL}/api/v1/auth`, {
                 method: 'POST',
-                headers: headers,
+                headers: myHeader,
                 body: JSON.stringify(loginDTO),
             });
-            console.log(response);
-
             if (!response.ok) throw new Error('The response failed');
-            return response.json(); */
-            const config = {
-                url: `${URL}/api/v1/auth`,
-                method: 'POST',
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    origin: 'x-requested-with',
-                    'Access-Control-Allow-Headers':
-                        'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
-                    'Content-Type': 'application/json',
-                },
-                data: JSON.stringify(loginDTO),
-            };
-            const response = await axios(config);
-            console.log(response);
-            return response;
+            return response.json();
         },
     };
 })();
