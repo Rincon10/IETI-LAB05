9727d72fcbdf09ba06fcfb47aeab4f125df0daa0
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-03-08 08:12:47 -0500

    Updating SpringBoot configuration

--- task-back-end/src/main/java/edu/eci/ieti/taskapp/TaskappApplication.java
+++ task-back-end/src/main/java/edu/eci/ieti/taskapp/TaskappApplication.java
@@ -2,12 +2,25 @@ package edu.eci.ieti.taskapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableMongoRepositories("edu.eci.ieti.taskapp.repository")
 public class TaskappApplication {
+	public static int getPort() {
+		if (System.getenv("PORT") != null) {
+			return Integer.parseInt(System.getenv("PORT"));
+		}
+		return 8080;
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaskappApplication.class, args);
+		SpringApplication app = new SpringApplication(TaskappApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+		app.run(args);
 	}
 
 }
